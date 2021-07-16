de19b3b1d9cb947e2e5a0ff0f059edee5aa19480
Yuzaki Tsukasa <violetnris@outlook.com>
2021-07-16 11:00:28 +0800

    重置最后最后一次提交

--- kwargs.py
+++ kwargs.py
@@ -1,11 +1,11 @@
 class DNN:
-    epochs = 5  # 训练轮次
+    epochs = 10  # 训练轮次
     predictPath = "D:/DataSource/train_data/arc"  # 预测场景选取
     judge_window = 100
-    test_model = "model.test.h5"
+    test_model = "model.h5"
 
 
-class predict:
+class pretreat:
     judge_val = 0.80  # 判断阈值
     predict_val_path = "./temp/预测值统计.temp.xls"
 
