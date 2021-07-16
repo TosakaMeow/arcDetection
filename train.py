56bc3b479665e91233333b2ee52c24b7959bae64
VioletNris <violetnris@outlook.com>
2021-07-16 10:25:27 +0800

    调整了模型结构，添加了池化层，准确度提升至91%

--- train.py
+++ train.py
@@ -1,14 +1,11 @@
-import pandas as pd
-import numpy as np
 import keras
-from keras.models import Sequential
 import keras.layers
 import matplotlib.pyplot as plt
-import kwargs
+import pandas as pd
 from keras.models import Model
 from sklearn.utils import shuffle
 
-
+import kwargs
 
 
 def train_main(path):
@@ -19,30 +16,30 @@ def train_main(path):
     data = shuffle(train_data)
 
     data = data[int(len(data) *0.2):]
-    conv_x = data[["area"]].values.reshape(len(data), 1, 1)
+    conv_x = data[["area", "low", "high"]].values.reshape(len(data), 1, 3)
     conv_y = data["label"].values.reshape(len(data), 1, 1)
-    base_x = data[["low", "high", "f2", "f3", "f4",
-                    "f5", "f6", "f7", "f8", "f9", "f10",
-                    "f12", "f13", "f14", "f15", "f16",
-                    "f17", "f18", "f19", "f20"]].values.reshape(len(data), 1, 20)
+    base_x = data[["f1", "f2", "f3", "f4", "f5",
+                   "f6", "f7", "f8", "f9", "f10",
+                   "f11","f12", "f13", "f14", "f15",
+                   "f16","f17", "f18", "f19", "f20"]].values.reshape(len(data), 1, 20)
     base_y = data["label"].values.reshape(len(data), 1)
 
     # 打乱数据
     evaluate_data = data[:int(len(data) *0.2)]
-    evaluate_conv_x = evaluate_data[["area"]].values.reshape(len(evaluate_data), 1, 1)
+    evaluate_conv_x = evaluate_data[["area", "low", "high"]].values.reshape(len(evaluate_data), 1, 3)
     evaluate_conv_y = evaluate_data["label"].values.reshape(len(evaluate_data), 1, 1)
-    evaluate_base_x = evaluate_data[["low", "high", "f2", "f3", "f4",
-                                     "f5", "f6", "f7", "f8", "f9", "f10",
-                                     "f12", "f13", "f14", "f15", "f16",
-                                     "f17", "f18", "f19", "f20"]].values.reshape(len(evaluate_data), 1, 20)
+    evaluate_base_x = evaluate_data[["f1", "f2", "f3", "f4", "f5",
+                                     "f6", "f7", "f8", "f9", "f10",
+                                     "f11","f12", "f13", "f14", "f15",
+                                     "f16","f17", "f18", "f19", "f20"]].values.reshape(len(evaluate_data), 1, 20)
     evaluate_base_y = evaluate_data["label"].values.reshape(len(evaluate_data), 1)
 
     # 对时域进行拟合
     conv_model = keras.Sequential()
+    conv_model.add(layer=keras.layers.Dense(32, activation='relu', input_shape=(None, 3)))
     conv_model.add(
-        layer=keras.layers.Conv1D(filters=4, kernel_size=1000, strides=100, padding='same', input_shape=(None, 1)))
-    # conv_model.add(layer=keras.layers.MaxPooling2D(pool_size=10))
-    conv_model.add(layer=keras.layers.Dense(32, activation='relu'))
+        layer=keras.layers.Conv1D(filters=6, kernel_size=100, strides=10, padding='same'))
+    conv_model.add(layer=keras.layers.MaxPooling1D(pool_size=6, strides=1, data_format='channels_first'))
     conv_model.add(layer=keras.layers.Dropout(0.5))
     conv_model.add(layer=keras.layers.Dense(32, activation='relu'))
     conv_model.add(layer=keras.layers.Dropout(0.5))
@@ -71,9 +68,9 @@ def train_main(path):
                        )
 
     # 训练模型
-    history = model.fit([base_x, conv_x], conv_y, epochs=kwargs.DNN.epochs, batch_size=8)
+    history = model.fit([base_x, conv_x], conv_y, epochs=kwargs.DNN.epochs, batch_size=16)
     print("正在进行验证......")
-    loss, accuracy = model.evaluate([evaluate_base_x, evaluate_conv_x], evaluate_conv_y, batch_size=8)
+    loss, accuracy = model.evaluate([evaluate_base_x, evaluate_conv_x], evaluate_conv_y, batch_size=16)
     print("验证集的准确率为：",accuracy)
 
 
